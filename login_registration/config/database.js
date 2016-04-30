e777a79a65136e50004d03c77618bd22df44deb4
Tomomi <tomomi.tamura2486@gmail.com>
2016-04-29 18:18:27 -0700

    initial commit views not yet working

---
+++ login_registration/config/database.js
@@ -0,0 +1,18 @@
+// config/database.js
+var mongoose = require('mongoose');
+var fs = require('fs');
+// use the heroku uri if it is there (only present when we deploy)
+// else use our default local mongodb uri
+var mongoDbURI = process.env.MONGODB_URI || 'mongodb://localhost/mean_store';
+
+mongoose.connect(mongoDbURI, function (error) {
+    if (error) console.error(error);
+    else console.log('mongo connected');
+});
+
+var models_path = __dirname + '/../app/models';
+fs.readdirSync(models_path).forEach(function(file){
+  if(file.indexOf('.js') > 0){
+    require(models_path+'/'+file);
+  }
+})
