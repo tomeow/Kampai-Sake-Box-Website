a705afc9a7674038f1c8ba4a70933bd8cf1b2a6f
Tomomi <tomomi.tamura2486@gmail.com>
2016-05-01 17:18:55 -0700

    get login functionality

--- login_registration/app/routes.js
+++ login_registration/app/routes.js
@@ -17,8 +17,8 @@ module.exports = function(app, passport) {
 
   // process the login form
   app.post('/login', passport.authenticate('local-login', {
-    successRedirect : '/profile', // redirect to the secure profile section
-    failureRedirect : '/login', // redirect back to the signup page if there is an error
+    successRedirect : './profile', // redirect to the secure profile section
+    failureRedirect : './login', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
   }));
 
